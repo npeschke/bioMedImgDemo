aebaef38c47ffab5560aba1cd2f5e11db032e8cb
Nicolas Peschke <peschke@stud.uni-heidelberg.de>
2019-05-06 22:53:21 +0200

    initial commit

---
+++ comms.py
@@ -0,0 +1,14 @@
+"""
+Author      Nicolas Peschke
+Date        06.05.2019
+"""
+
+
+def hello_world():
+    """
+    This method prints the string "Hello World" to the console
+    :return: String "Hello World"
+    """
+    hello = "Hello World"
+    print(hello)
+    return hello
