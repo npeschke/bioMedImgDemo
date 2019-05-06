b2561fd96b6f21ccb41b56ed36e68bb39e0197e9
Nicolas Peschke <peschke@stud.uni-heidelberg.de>
2019-05-06 23:03:46 +0200

    initial commit of helloWorld.py

---
+++ helloWorld.py
@@ -0,0 +1,12 @@
+"""
+Author      Nicolas Peschke
+Date        06.05.2019
+"""
+# This does the same as the helloWorld Jupyter Notebook
+
+# Importing the self written module
+import comms
+
+# Running the code
+if __name__ == '__main__':
+    comms.hello_world()
